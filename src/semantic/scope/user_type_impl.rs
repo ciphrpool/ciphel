d44cb62a3466e3fe6f743bae10af2f9300f2c1de
Willem <darwil.nicolas@gmail.com>
2024-01-05 16:01:55 +0100

    start of implementation of user_type_impl

--- src/semantic/scope/user_type_impl.rs
+++ src/semantic/scope/user_type_impl.rs
@@ -1,6 +1,15 @@
-use crate::semantic::{CompatibleWith, EitherType, MergeType, SemanticError, TypeOf};
+use std::collections::{HashMap, HashSet};
+
+use crate::{
+    ast::{
+        statements::definition::{self, StructDef},
+        utils::strings::ID,
+    },
+    semantic::{CompatibleWith, EitherType, MergeType, SemanticError, TypeOf},
+};
 
 use super::{
+    static_type_impl::StaticType,
     type_traits::{GetSubTypes, IsEnum, OperandMerging, TypeChecking},
     BuildUserType, ScopeApi,
 };
@@ -13,13 +22,34 @@ pub enum UserType {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Struct();
+pub struct Struct {
+    id: ID,
+    fields: StructVariant,
+}
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Enum();
+pub enum StructVariant {
+    Fields(HashMap<ID, EitherType<UserType, StaticType>>),
+    Inline(Vec<EitherType<UserType, StaticType>>),
+}
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Union();
+pub struct Enum {
+    id: ID,
+    values: HashSet<ID>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Union {
+    id: ID,
+    variants: HashMap<ID, UnionVariant>,
+}
+#[derive(Debug, Clone, PartialEq)]
+pub enum UnionVariant {
+    Id,
+    Fields(HashMap<ID, EitherType<UserType, StaticType>>),
+    Inline(Vec<EitherType<UserType, StaticType>>),
+}
 
 impl<Scope: ScopeApi<UserType = Self>> TypeOf<Scope> for UserType {
     fn type_of(&self, _scope: &Scope) -> Result<EitherType<Self, Scope::StaticType>, SemanticError>
@@ -27,57 +57,169 @@ impl<Scope: ScopeApi<UserType = Self>> TypeOf<Scope> for UserType {
         Scope: super::ScopeApi,
         Self: Sized,
     {
-        todo!()
+        Ok(EitherType::User(self.clone()))
     }
 }
 
-impl<Scope: ScopeApi<UserType = Self>> CompatibleWith<Scope> for UserType {
-    fn compatible_with<Other>(&self, _other: &Other, _scope: &Scope) -> Result<(), SemanticError>
+impl<Scope: ScopeApi<StaticType = StaticType, UserType = Self>> CompatibleWith<Scope> for UserType {
+    fn compatible_with<Other>(&self, other: &Other, scope: &Scope) -> Result<(), SemanticError>
     where
         Other: TypeOf<Scope>,
     {
-        todo!()
+        match self {
+            UserType::Struct(value) => value.compatible_with(other, scope),
+            UserType::Enum(value) => value.compatible_with(other, scope),
+            UserType::Union(value) => value.compatible_with(other, scope),
+        }
     }
 }
 
 impl<Scope: ScopeApi<UserType = Self>> BuildUserType<Scope> for UserType {
-    fn build_usertype(_type_sig: &crate::ast::statements::definition::TypeDef) -> Self {
+    fn build_usertype(type_sig: &crate::ast::statements::definition::TypeDef) -> Self {
+        match type_sig {
+            definition::TypeDef::Struct(value) => UserType::Struct(Struct::build(value)),
+            definition::TypeDef::Union(value) => UserType::Union(Union::build(value)),
+            definition::TypeDef::Enum(value) => UserType::Enum(Enum::build(value)),
+        }
+    }
+}
+
+impl<Scope: ScopeApi<UserType = Self>> GetSubTypes<Scope> for UserType {
+    fn get_nth(&self, _n: &usize) -> Option<EitherType<Scope::UserType, Scope::StaticType>> {
+        todo!()
+    }
+    fn get_field(&self, _field_id: &ID) -> Option<EitherType<Scope::UserType, Scope::StaticType>> {
+        todo!()
+    }
+    fn get_variant(&self, _variant: &ID) -> Option<EitherType<Scope::UserType, Scope::StaticType>> {
+        todo!()
+    }
+    fn get_item(&self) -> Option<EitherType<Scope::UserType, Scope::StaticType>> {
+        todo!()
+    }
+    fn get_return(&self) -> Option<EitherType<Scope::UserType, Scope::StaticType>> {
+        todo!()
+    }
+    fn get_fields(
+        &self,
+    ) -> Option<
+        Vec<(
+            Option<String>,
+            EitherType<Scope::UserType, Scope::StaticType>,
+        )>,
+    > {
         todo!()
     }
 }
 
-impl<Scope: ScopeApi<UserType = Self>> GetSubTypes<Scope> for UserType {}
-
-impl<Scope: ScopeApi<UserType = Self>> TypeChecking<Scope> for UserType {}
+impl<Scope: ScopeApi<UserType = Self>> TypeChecking<Scope> for UserType {
+    fn is_enum_variant(&self) -> bool {
+        todo!()
+    }
+}
 
 impl<Scope: ScopeApi<UserType = Self>> OperandMerging<Scope> for UserType {}
 
-impl IsEnum for UserType {}
+impl IsEnum for UserType {
+    fn is_enum(&self) -> bool {
+        match self {
+            UserType::Struct(_) => false,
+            UserType::Enum(_) => true,
+            UserType::Union(_) => false,
+        }
+    }
+}
 
-impl<Scope: ScopeApi<UserType = UserType>> MergeType<Scope> for UserType {
+impl<Scope: ScopeApi<StaticType = StaticType, UserType = UserType>> MergeType<Scope> for UserType {
     fn merge<Other>(
         &self,
-        _other: &Other,
-        _scope: &Scope,
+        other: &Other,
+        scope: &Scope,
     ) -> Result<EitherType<UserType, <Scope as ScopeApi>::StaticType>, SemanticError>
     where
         Other: TypeOf<Scope>,
     {
+        let other_type = other.type_of(scope)?;
+        let EitherType::User(other_type) = other_type else {
+            return Err(SemanticError::IncompatibleTypes);
+        };
+        match self {
+            UserType::Struct(value) => match other_type {
+                UserType::Struct(other_type) => value
+                    .merge(&other_type)
+                    .map(|v| EitherType::User(UserType::Struct(v))),
+                _ => Err(SemanticError::IncompatibleTypes),
+            },
+            UserType::Enum(value) => match other_type {
+                UserType::Enum(other_type) => value
+                    .merge(&other_type)
+                    .map(|v| EitherType::User(UserType::Enum(v))),
+                _ => Err(SemanticError::IncompatibleTypes),
+            },
+            UserType::Union(value) => match other_type {
+                UserType::Union(other_type) => value
+                    .merge(&other_type)
+                    .map(|v| EitherType::User(UserType::Union(v))),
+                _ => Err(SemanticError::IncompatibleTypes),
+            },
+        }
+    }
+}
+
+impl<Scope: ScopeApi<StaticType = StaticType, UserType = UserType>> CompatibleWith<Scope>
+    for Struct
+{
+    fn compatible_with<Other>(&self, other: &Other, scope: &Scope) -> Result<(), SemanticError>
+    where
+        Other: TypeOf<Scope>,
+    {
         todo!()
     }
 }
 
-impl<Scope: ScopeApi<UserType = UserType>> CompatibleWith<Scope> for Enum {
+impl<Scope: ScopeApi<StaticType = StaticType, UserType = UserType>> CompatibleWith<Scope>
+    for Union
+{
+    fn compatible_with<Other>(&self, other: &Other, scope: &Scope) -> Result<(), SemanticError>
+    where
+        Other: TypeOf<Scope>,
+    {
+        todo!()
+    }
+}
+impl<Scope: ScopeApi<StaticType = StaticType, UserType = UserType>> CompatibleWith<Scope> for Enum {
     fn compatible_with<Other>(&self, _other: &Other, _scope: &Scope) -> Result<(), SemanticError>
     where
         Other: TypeOf<Scope>,
     {
+        todo!()
+    }
+}
+
+impl Struct {
+    pub fn build(from: &definition::StructDef) -> Self {
+        todo!()
+    }
+
+    pub fn merge(&self, _other: &Self) -> Result<Self, SemanticError> {
+        todo!()
+    }
+}
+
+impl Union {
+    pub fn build(from: &definition::UnionDef) -> Self {
+        todo!()
+    }
+    pub fn merge(&self, _other: &Self) -> Result<Self, SemanticError> {
         todo!()
     }
 }
 
 impl Enum {
-    pub fn merge(&self, _other: &Enum) -> Result<Enum, SemanticError> {
+    pub fn build(from: &definition::EnumDef) -> Self {
+        todo!()
+    }
+    pub fn merge(&self, _other: &Self) -> Result<Self, SemanticError> {
         todo!()
     }
 }
